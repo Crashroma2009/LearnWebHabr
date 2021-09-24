43082274e20e7a90332afdff4ce4cc9f894f594d
Roman Deynega <Crashroma2009@yandex.ru>
2021-09-24 19:53:24 +0300

    Flask-Migrations и регистрация пользователей

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.fields.simple import SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={'class':'form-control'})
-    password = PasswordField('пароль', validators=[DataRequired()], render_kw={'class':'form-control'})
-    remember_me = BooleanField('запомнить меня', default=True, render_kw={"class": "form-check-input"})
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={'class':'form-control'})
+    remember_me = BooleanField('Запомнить меня', default=True, render_kw={"class": "form-check-input"})
     submit = SubmitField('Отправить', render_kw={'class': 'btn-primary'})
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={'class':'form-control'})
+    email = StringField('Электронная почта', validators=[DataRequired(), Email()], render_kw={'class':'form-control'})
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={'class':'form-control'})
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')], render_kw={'class':'form-control'})
+    submit = SubmitField('Отправить', render_kw={'class': 'btn-primary'})
+
